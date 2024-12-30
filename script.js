bfec49243c82bd492c06829caf1e9875474738f3
areebalii <areebktk8@gmail.com>
2024-12-29 18:01:53 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select elements
+const menuIcon = document.getElementById('menu');
+const navMenu = document.querySelector('.links');
+
+// Add click event to toggle menu visibility
+menuIcon.addEventListener('click', () => { 
+  navMenu.classList.toggle('active');
+});
