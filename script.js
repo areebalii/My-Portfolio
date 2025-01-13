fe08ddd8670659ce0c8fad460ad60e7d8ad5c796
areebalii <areebktk8@gmail.com>
2025-01-13 01:44:24 -0800

    changes in js

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ const navMenu = document.querySelector('.links');
 
 // Add click event to toggle menu visibility
 menuIcon.addEventListener('click', () => { 
-  navMenu.classList.toggle('active');
+  navMenu.classList.add('active');
 });
+navMenu.addEventListener("click", () => {
+  navMenu.classList.remove('active');
+})
